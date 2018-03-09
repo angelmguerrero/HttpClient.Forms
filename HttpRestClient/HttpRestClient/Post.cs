dd1b5a1ba21d1fef598d4874a6f3bd477b53bb86
Luis David De la cruz <delacruz_luis@outlook.es>
2018-03-09 11:57:42 -0600

    Xamarin Forms Solutin

---
+++ HttpRestClient/HttpRestClient/Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HttpRestClient
+{
+    internal class Post: INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        private string _title;
+        
+        [JsonProperty("title")]
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                //OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName= null)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
